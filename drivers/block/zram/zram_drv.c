989624574a8ecfa7584f86f6aa8afa744f1c884d
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2015-04-22 01:03:45 +0000

    zram: return zram device_id value from zram_add()
    zram_add requires valid device_id to be provided, that can be a bit
    inconvenient.  Change zram_add() to return negative value upon new device
    creation failure, and device_id (>= 0) value otherwise.
    
    This prepares zram_add to perform automatic device_id assignment.  New
    device_id will be returned back to user-space (so user can reference that
    device as /dev/zramX).
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1128,6 +1128,8 @@ static struct attribute_group zram_disk_attr_group = {
 	.attrs = zram_disk_attrs,
 };
 
+/* allocate and initialize new zram device. the function returns
+ * '>= 0' device_id upon success, and negative value otherwise. */
 static int zram_add(int device_id)
 {
 	struct zram *zram;
@@ -1214,7 +1216,7 @@ static int zram_add(int device_id)
 	zram->max_comp_streams = 1;
 
 	pr_info("Added device: %s\n", zram->disk->disk_name);
-	return 0;
+	return device_id;
 
 out_free_disk:
 	del_gendisk(zram->disk);
@@ -1389,7 +1391,7 @@ static int __init zram_init(void)
 		mutex_lock(&zram_index_mutex);
 		ret = zram_add(dev_id);
 		mutex_unlock(&zram_index_mutex);
-		if (ret != 0)
+		if (ret < 0)
 			goto out_error;
 	}
 
