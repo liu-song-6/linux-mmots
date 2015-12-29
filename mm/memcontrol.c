d6998d930660c69e2bb31ee2416596de1416d6c3
Johannes Weiner <hannes@cmpxchg.org>
2015-12-29 01:00:53 +0000

    mm: memcontrol: fix possible memcg leak due to interrupted reclaim fix
    When we handled multiple css references in mem_cgroup_iter() it got a
    little confusing which puts belong to which gets. Now that we only ref
    the position css it's obvious, and a multi-line comment to explain it
    gets in the way of reading an already long function.
    
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -969,10 +969,6 @@ struct mem_cgroup *mem_cgroup_iter(struct mem_cgroup *root,
 		 */
 		(void)cmpxchg(&iter->position, pos, memcg);
 
-		/*
-		 * pairs with css_tryget when dereferencing iter->position
-		 * above.
-		 */
 		if (pos)
 			css_put(&pos->css);
 
