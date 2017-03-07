c61311412e1b54c459d308c9a4947f2538c2931d
Laura Abbott <labbott@redhat.com>
2017-03-07 01:01:36 +0000

    module: use set_memory.h header
    set_memory_* functions have moved to set_memory.h. Switch to this
    explicitly.
    
    Link: http://lkml.kernel.org/r/1488413706-9739-12-git-send-email-labbott@redhat.com
    Signed-off-by: Laura Abbott <labbott@redhat.com>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: David Airlie <airlied@linux.ie>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Wim Van Sebroeck <wim@iguana.be>
    Cc: Guenter Roeck <linux@roeck-us.net>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: Jessica Yu <jeyu@redhat.com>
    Cc: Takashi Iwai <tiwai@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/module.c
+++ kernel/module.c
@@ -49,6 +49,7 @@
 #include <linux/rculist.h>
 #include <linux/uaccess.h>
 #include <asm/cacheflush.h>
+#include <asm/set_memory.h>
 #include <asm/mmu_context.h>
 #include <linux/license.h>
 #include <asm/sections.h>
