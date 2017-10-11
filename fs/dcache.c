af57df90cf03371520d94ed0785eaa6c3e49cfa8
Andrew Morton <akpm@linux-foundation.org>
2017-10-11 00:01:38 +0000

    linux-next-rejects
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/dcache.c
+++ fs/dcache.c
@@ -3641,8 +3641,9 @@ void __init vfs_caches_init_early(void)
 
 void __init vfs_caches_init(void)
 {
-	names_cachep = kmem_cache_create("names_cache", PATH_MAX, 0,
-			SLAB_HWCACHE_ALIGN|SLAB_PANIC|SLAB_ACCOUNT, NULL);
+	names_cachep = kmem_cache_create_usercopy("names_cache", PATH_MAX, 0,
+			SLAB_HWCACHE_ALIGN|SLAB_PANIC|SLAB_ACCOUNT, 0, PATH_MAX,
+			NULL);
 
 	dcache_init();
 	inode_init();
