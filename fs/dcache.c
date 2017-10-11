b788b3a28d157ba8df6e5d11ed89b6949fa68b1f
Shakeel Butt <shakeelb@google.com>
2017-10-11 00:01:19 +0000

    fs, mm: account filp and names caches to kmemcg
    The allocations from filp and names kmem caches can be directly triggered
    by user space applications.  A buggy application can consume a significant
    amount of unaccounted system memory.  Though we have not noticed such
    buggy applications in our production but upon close inspection, we found
    that a lot of machines spend very significant amount of memory on these
    caches.  So, these caches should be accounted to kmemcg.
    
    Link: http://lkml.kernel.org/r/20171005222144.123797-1-shakeelb@google.com
    Signed-off-by: Shakeel Butt <shakeelb@google.com>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Greg Thelen <gthelen@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/dcache.c
+++ fs/dcache.c
@@ -3641,7 +3641,7 @@ void __init vfs_caches_init_early(void)
 void __init vfs_caches_init(void)
 {
 	names_cachep = kmem_cache_create("names_cache", PATH_MAX, 0,
-			SLAB_HWCACHE_ALIGN|SLAB_PANIC, NULL);
+			SLAB_HWCACHE_ALIGN|SLAB_PANIC|SLAB_ACCOUNT, NULL);
 
 	dcache_init();
 	inode_init();
