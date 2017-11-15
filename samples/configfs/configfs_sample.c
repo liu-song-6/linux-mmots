1654747c23a4dca97c9db97e14b0d070a11c78b3
Martin Kepplinger <martink@posteo.de>
2017-11-15 01:02:50 +0000

    samples: replace FSF address with web source in license notices
    A few years ago the FSF moved and "59 Temple Place" is wrong.  Having this
    still in our source files feels old and unmaintained.
    
    Let's take the license statement serious and not confuse users.
    
    As https://www.gnu.org/licenses/gpl-howto.html suggests, we replace the
    postal address with "<http://www.gnu.org/licenses/>" in the samples
    directory.
    
    Link: http://lkml.kernel.org/r/20171114095037.28563-1-martink@posteo.de
    Signed-off-by: Martin Kepplinger <martink@posteo.de>
    Cc: Greg KH <greg@kroah.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- samples/configfs/configfs_sample.c
+++ samples/configfs/configfs_sample.c
@@ -15,10 +15,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public
- * License along with this program; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 021110-1307, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Based on sysfs:
  * 	sysfs is Copyright (C) 2001, 2002, 2003 Patrick Mochel
