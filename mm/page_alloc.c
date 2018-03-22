c3f13c12be8f093f7e65042bdec45f897bbd709c
Aaron Lu <aaron.lu@intel.com>
2018-03-22 00:02:01 +0000

    mm-free_pcppages_bulk-prefetch-buddy-while-not-holding-lock-v4-update2
    Use a helper function to prefetch buddy as suggested by Dave Hansen.
    Drop the change of list_add_tail() to avoid disordering page.
    
    Link: http://lkml.kernel.org/r/20180301062845.26038-4-aaron.lu@intel.com
    Link: http://lkml.kernel.org/r/20180320113146.GB24737@intel.com
    Signed-off-by: Aaron Lu <aaron.lu@intel.com>
    Suggested-by: Ying Huang <ying.huang@intel.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1063,6 +1063,15 @@ static bool bulkfree_pcp_prepare(struct page *page)
 }
 #endif /* CONFIG_DEBUG_VM */
 
+static inline void prefetch_buddy(struct page *page)
+{
+	unsigned long pfn = page_to_pfn(page);
+	unsigned long buddy_pfn = __find_buddy_pfn(pfn, 0);
+	struct page *buddy = page + (buddy_pfn - pfn);
+
+	prefetch(buddy);
+}
+
 /*
  * Frees a number of pages from the PCP lists
  * Assumes all pages on list are in same zone, and of same order.
@@ -1079,6 +1088,7 @@ static void free_pcppages_bulk(struct zone *zone, int count,
 {
 	int migratetype = 0;
 	int batch_free = 0;
+	int prefetch_nr = 0;
 	bool isolated_pageblocks;
 	struct page *page, *tmp;
 	LIST_HEAD(head);
@@ -1105,9 +1115,6 @@ static void free_pcppages_bulk(struct zone *zone, int count,
 			batch_free = count;
 
 		do {
-			unsigned long pfn, buddy_pfn;
-			struct page *buddy;
-
 			page = list_last_entry(list, struct page, lru);
 			/* must delete to avoid corrupting pcp list */
 			list_del(&page->lru);
@@ -1116,7 +1123,7 @@ static void free_pcppages_bulk(struct zone *zone, int count,
 			if (bulkfree_pcp_prepare(page))
 				continue;
 
-			list_add(&page->lru, &head);
+			list_add_tail(&page->lru, &head);
 
 			/*
 			 * We are going to put the page back to the global
@@ -1125,14 +1132,10 @@ static void free_pcppages_bulk(struct zone *zone, int count,
 			 * an additional test and calculating buddy_pfn here
 			 * can be offset by reduced memory latency later. To
 			 * avoid excessive prefetching due to large count, only
-			 * prefetch buddy for the last pcp->batch nr of pages.
+			 * prefetch buddy for the first pcp->batch nr of pages.
 			 */
-			if (count > pcp->batch)
-				continue;
-			pfn = page_to_pfn(page);
-			buddy_pfn = __find_buddy_pfn(pfn, 0);
-			buddy = page + (buddy_pfn - pfn);
-			prefetch(buddy);
+			if (prefetch_nr++ < pcp->batch)
+				prefetch_buddy(page);
 		} while (--count && --batch_free && !list_empty(list));
 	}
 
