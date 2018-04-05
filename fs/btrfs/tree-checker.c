988cdbefca070ece5b2a68230e5fe8e53a29b19d
Kees Cook <keescook@chromium.org>
2018-04-05 02:04:44 +0000

    treewide: remove false-positive VLAs when using max()
    As part of removing VLAs from the kernel[1], we want to build with -Wvla,
    but it is overly pessimistic and only accepts constant expressions for
    stack array sizes, instead of also constant values.  The max() macro
    triggers the warning, so this refactors these uses of max() to use the new
    const_max() instead.
    
    [1] https://lkml.org/lkml/2018/3/7/621
    
    Link: http://lkml.kernel.org/r/1521143266-31350-3-git-send-email-keescook@chromium.org
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Nikolay Borisov <nborisov@suse.com> [btrfs]
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: David Laight <David.Laight@aculab.com>
    Cc: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/btrfs/tree-checker.c
+++ fs/btrfs/tree-checker.c
@@ -341,7 +341,8 @@ static int check_dir_item(struct btrfs_root *root,
 		 */
 		if (key->type == BTRFS_DIR_ITEM_KEY ||
 		    key->type == BTRFS_XATTR_ITEM_KEY) {
-			char namebuf[max(BTRFS_NAME_LEN, XATTR_NAME_MAX)];
+			char namebuf[const_max(BTRFS_NAME_LEN,
+					       XATTR_NAME_MAX)];
 
 			read_extent_buffer(leaf, namebuf,
 					(unsigned long)(di + 1), name_len);
