476d8c4375b001024a5fc4727aeb934b716eab01
Nikolay Borisov <nborisov@suse.com>
2018-04-07 01:03:25 +0000

    fs/dcache.c: add cond_resched() in shrink_dentry_list()
    As previously reported (https://patchwork.kernel.org/patch/8642031/) it's
    possible to call shrink_dentry_list with a large number of dentries (>
    10000).  This, in turn, could trigger the softlockup detector and possibly
    trigger a panic.  In addition to the unmount path being vulnerable to this
    scenario, at SuSE we've observed similar situation happening during
    process exit on processes that touch a lot of dentries.  Here is an
    excerpt from a crash dump.  The number after the colon are the number of
    dentries on the list passed to shrink_dentry_list:
    
    PID 99760: 10722
    PID 107530: 215
    PID 108809: 24134
    PID 108877: 21331
    PID 141708: 16487
    
    So we want to kill between 15k-25k dentries without yielding.
    
    And one possible call stack looks like:
    
    4 [ffff8839ece41db0] _raw_spin_lock at ffffffff8152a5f8
    5 [ffff8839ece41db0] evict at ffffffff811c3026
    6 [ffff8839ece41dd0] __dentry_kill at ffffffff811bf258
    7 [ffff8839ece41df0] shrink_dentry_list at ffffffff811bf593
    8 [ffff8839ece41e18] shrink_dcache_parent at ffffffff811bf830
    9 [ffff8839ece41e50] proc_flush_task at ffffffff8120dd61
    10 [ffff8839ece41ec0] release_task at ffffffff81059ebd
    11 [ffff8839ece41f08] do_exit at ffffffff8105b8ce
    12 [ffff8839ece41f78] sys_exit at ffffffff8105bd53
    13 [ffff8839ece41f80] system_call_fastpath at ffffffff81532909
    
    While some of the callers of shrink_dentry_list do use cond_resched, this
    is not sufficient to prevent softlockups.  So just move cond_resched into
    shrink_dentry_list from its callers.
    
    David said: I've found hundreds of occurrences of warnings that we emit
    when need_resched stays set for a prolonged period of time with the
    stack trace that is included in the change log.
    
    Link: http://lkml.kernel.org/r/1521718946-31521-1-git-send-email-nborisov@suse.com
    Signed-off-by: Nikolay Borisov <nborisov@suse.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: David Rientjes <rientjes@google.com>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Goldwyn Rodrigues <rgoldwyn@suse.de>
    Cc: Jeff Mahoney <jeffm@suse.com>
    Cc: Davidlohr Bueso <dave@stgolabs.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/dcache.c
+++ fs/dcache.c
@@ -1053,6 +1053,8 @@ static void shrink_dentry_list(struct list_head *list)
 	while (!list_empty(list)) {
 		struct dentry *dentry, *parent;
 
+		cond_resched();
+
 		dentry = list_entry(list->prev, struct dentry, d_lru);
 		spin_lock(&dentry->d_lock);
 		rcu_read_lock();
@@ -1206,7 +1208,6 @@ void shrink_dcache_sb(struct super_block *sb)
 
 		this_cpu_sub(nr_dentry_unused, freed);
 		shrink_dentry_list(&dispose);
-		cond_resched();
 	} while (list_lru_count(&sb->s_dentry_lru) > 0);
 }
 EXPORT_SYMBOL(shrink_dcache_sb);
@@ -1488,7 +1489,6 @@ void shrink_dcache_parent(struct dentry *parent)
 			break;
 
 		shrink_dentry_list(&data.dispose);
-		cond_resched();
 	}
 }
 EXPORT_SYMBOL(shrink_dcache_parent);
@@ -1615,7 +1615,6 @@ void d_invalidate(struct dentry *dentry)
 			detach_mounts(data.mountpoint);
 			dput(data.mountpoint);
 		}
-		cond_resched();
 	}
 }
 EXPORT_SYMBOL(d_invalidate);
