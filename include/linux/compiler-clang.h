1c5f64a5418454dbcf8a3ef80e83ab4ee6142d48
Andrey Konovalov <andreyknvl@google.com>
2018-04-18 02:01:15 +0000

    kasan-add-no_sanitize-attribute-for-clang-builds-v2
    remove redundant #ifdef CONFIG_KASAN check
    
    Link: http://lkml.kernel.org/r/c79aa31a2a2790f6131ed607c58b0dd45dd62a6c.1523967959.git.andreyknvl@google.com
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Cc: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/compiler-clang.h
+++ include/linux/compiler-clang.h
@@ -25,10 +25,8 @@
 #define __SANITIZE_ADDRESS__
 #endif
 
-#ifdef CONFIG_KASAN
 #undef __no_sanitize_address
 #define __no_sanitize_address __attribute__((no_sanitize("address")))
-#endif
 
 /* Clang doesn't have a way to turn it off per-function, yet. */
 #ifdef __noretpoline
